6e5adda27724d960dba9780fd63cfb04c6331425
tframe98 <tylerframe8@gmail.com>
2025-08-06 12:54:33 -0700

    middlware fixed

--- middleware.ts
+++ middleware.ts
@@ -1,24 +1,28 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-
 const isPublicRoute = createRouteMatcher([
   "/",
   "/login(.*)",
   "/register(.*)",
-  "book(.*)",
-])
+  "/book(.*)",
+  "/event(.*)",
+]);
 
-export default clerkMiddleware(async(auth, req) => {
+export default clerkMiddleware((auth, req) => {
   if (!isPublicRoute(req)) {
-    await auth.protect()
+    auth.protect();
   }
 });
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - public folder
+     */
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 };
